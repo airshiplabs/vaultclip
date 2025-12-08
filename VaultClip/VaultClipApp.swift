5858abea7c8463582bebca918b8b43bb1cc67378
Matthew Boston <matthew@matthewboston.com>
2025-12-08 08:31:14 -0700

    feat: add encryption data structures

--- VaultClip/VaultClipApp.swift
+++ VaultClip/VaultClipApp.swift
@@ -1,4 +1,26 @@
 import SwiftUI
+import CryptoKit
+import AppKit
+import Carbon.HIToolbox
+import Combine
+
+// MARK: - Encryption Data Structures
+
+struct EncryptedClipboardItem: Identifiable {
+    let id: UUID
+    let timestamp: Date
+    let ciphertext: Data      // Encrypted text
+    let nonce: Data           // AES.GCM.Nonce (12 bytes)
+    let tag: Data             // Authentication tag (16 bytes)
+}
+
+enum EncryptionError: Error {
+    case invalidInput
+    case keyRetrievalFailed
+    case encryptionFailed
+    case decryptionFailed
+    case corruptedData
+}
 
 @main
 struct VaultClipApp: App {
